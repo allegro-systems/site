4880753b9b92f7f190f599ccd3c032c907b73186
Mac <maclong9@icloud.com>
2026-03-20 01:40:28 +0000

    Use shared AllegroTheme plugin instead of local theme

--- Package.swift
+++ Package.swift
@@ -5,13 +5,17 @@ let package = Package(
     name: "AllegroSite",
     platforms: [.macOS(.v14)],
     dependencies: [
-        .package(path: "../score")
+        .package(path: "../score"),
+        .package(path: "../score-plugins/score-lucide"),
+        .package(path: "../score-plugins/allegro-theme"),
     ],
     targets: [
         .executableTarget(
             name: "AllegroSite",
             dependencies: [
-                .product(name: "Score", package: "Score")
+                .product(name: "Score", package: "Score"),
+                .product(name: "ScoreLucide", package: "score-lucide"),
+                .product(name: "AllegroTheme", package: "allegro-theme"),
             ],
             path: "Sources"
         )
--- Sources/App.swift
+++ Sources/App.swift
@@ -1,11 +1,22 @@
 import Score
+import ScoreLucide
+import AllegroTheme
 
 /// The Allegro Systems marketing and documentation site.
 @main
 struct AllegroSite: Application {
     var theme: (any Theme)? { AllegroTheme() }
 
+    var plugins: [any ScorePlugin] { [LucidePlugin()] }
+
     var metadata: (any Metadata)? { nil }
+
+    // swiftlint:disable:next force_try
+    private static let _localization: Localization? = try! Localization(
+        catalog: StringCatalog.load(from: "Localizable.xcstrings")
+    )
+
+    var localization: Localization? { Self._localization }
 
     @PageBuilder
     var pages: [any Page] {
@@ -16,8 +27,12 @@ struct AllegroSite: Application {
         ComposerPage()
         LibrettoPage()
         DocsPage()
+        ScoreDocsPages()
+        StageDocsPage()
+        ComposerDocsPage()
         AboutPage()
         BlogPage()
+        BlogPostPages()
     }
 
     var errorPage: (any ErrorPage.Type)? { NotFoundPage.self }
